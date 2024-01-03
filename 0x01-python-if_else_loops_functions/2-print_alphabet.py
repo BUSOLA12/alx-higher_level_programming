2439edf64174758e22d39d69d5256b32a384bf7b
BUSOLA12 <iyiolaolubusola@gmail.com>
2024-01-03 14:48:03 -0800

    made some changes

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """print the lowercase alphabets in string format"""
 for l_alp in range(ord('a'), ord('z') + 1):
-    print(f"{chr(l_alp)}", end="")
+    print("{}".format(chr(l_alp)), end="")
