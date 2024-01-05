af76dc350169401423928bcf88d38d0f7e651ac7
BUSOLA12 <iyiolaolubusola@gmail.com>
2024-01-05 15:00:31 -0800

    wrote a code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    import sys
 
-    argv_len = len(argv)
+    argv_len = len(sys.argv)
 
     if argv_len == 0:
         print("{} arguments.".format(argv_len))
     else:
         print("{} argument:".format(argv_len))
 
-    for i in range(1, argv_len + 1)
-        print("{}: {}".format(i, argv[i]))
+    for i in range(1, argv_len + 1):
+        print("{}: {}".format(i, sys.argv[i]))
