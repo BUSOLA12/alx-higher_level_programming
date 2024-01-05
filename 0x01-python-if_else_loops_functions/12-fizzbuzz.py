88a14ed50dbb5c09c71083b5be8da96fed106747
BUSOLA12 <iyiolaolubusola@gmail.com>
2024-01-05 10:28:41 -0800

    wrote a code

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 
 def fizzbuzz():
     for num in range(1, 101):
-        if num % 3 == 0:
-            print("{}".format("Fuzz"), end=" ")
+        if num % 3 == 0 and num % 5 == 0:
+            print("{}".format("FizzBuzz"), end=" ")
+        elif num % 3 == 0:
+            print("{}".format("Fizz"), end=" ")
         elif num % 5 == 0:
             print("{}".format("Buzz"), end=" ")
-        elif num % 3 == 0 and num % 5 == 0:
-            print("{}".format("FizzBuzz"), end=" ")
         else:
             print("{}".format(num), end=" ")
