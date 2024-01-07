6e9d5a1e40a9ace17600a4f13f2a3935a5ce013f
BUSOLA12 <iyiolaolubusola@gmail.com>
2024-01-07 01:15:39 -0800

    wrote a code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if count - 1 < 3 or count - 1 > 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
+    ops{"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
         print("Uknown operator. Available operators: +, -, * and /")
         sys.exit(1)
