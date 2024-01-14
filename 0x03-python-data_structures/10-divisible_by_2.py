d2bf0a242f0cf0ff0dc5fe878ecdebeb76c9157d
BUSOLA12 <iyiolaolubusola@gmail.com>
2024-01-14 04:57:53 -0800

    wrote a code

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new = [x % 2 == 0 for x in my_list]
+    new = []
+
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new.append(True)
+        else:
+            new.append(False)
     return new
