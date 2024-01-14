dac8924467d16e7b833da5fb3aa3dfe23427d8f9
BUSOLA12 <iyiolaolubusola@gmail.com>
2024-01-14 02:47:59 -0800

    wrote a code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
     def multiple_returns(sentence):
-        if sentence == "":
-            tuple_ = (0, None)
-        return tuple_
         count = len(sentence)
+        if count == 0:
+            tuple_ = (count, None)
+        return tuple_
         first_char = sentence[0]
         tuple_ = (count, first_char)
         return tuple_
