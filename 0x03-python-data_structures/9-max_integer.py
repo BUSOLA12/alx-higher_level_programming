cb317abb332e82c05102fa52457017fd7444396c
BUSOLA12 <iyiolaolubusola@gmail.com>
2024-01-14 03:32:28 -0800

    wrote a code

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,11 +3,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    else:
-        max_ = 0
-        for i in range(len(my_list) - 1):
-            if max_ > my_list[i]:
-                max_ = max_
-            else:
-                max_ = my_list[i]
+    max_ = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > max_:
+            max_ = my_list[i]
     return max_
