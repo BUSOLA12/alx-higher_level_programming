e6f27bfd4aecf217e972b0fe7fc12bae353187f0
BUSOLA12 <iyiolaolubusola@gmail.com>
2024-01-24 00:21:34 -0800

    wrote a code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 
 def search_replace(my_list, search, replace):
 
-    result = [my_list.replace(x, replace) for x in my_list if x == search]
+    result = [x = replace for x in my_list if x == search]
     return result
